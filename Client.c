78abfe2cb488f268539675b11eb3a360f0638fd8
mohitprajapati17 <mohitprajapati2444@gmail.com>
2026-06-24 01:00:36 +0530

    client ready

--- Client.c
+++ Client.c
@@ -13,5 +13,43 @@ static void die(const char *msg){
     int err=errno;
     fprintf(stderr,"[%d] %s \n",err,msg);
     abort();
+}
 
+int  main(){
+
+    int fd=socekt(AF_INET , SOCK_STREAM , 0);
+    if(fd<0){
+        die("Socket()");
+    }
+
+    struct sockaddr_in addr={};
+    addr.sin_family = AF_INET;
+    addr.sin_port=ntohs(12345);
+    addr.sin_addr.s_addr=ntohl(INADDR_LOOPBACK);
+    int rv=connect(fd , (struct sockaddr *)&addr , sizeof(addr));
+    if(rv<0){
+        die("connect()");
+    }
+
+    char msg[]="Hello Server";
+    write(fd,msg, strlen(msg));
+
+    char rbuf[64]=[];
+    ssize_t n=read(fd, rbuf,sizeof(rbuf)-1);
+    
+    if(n<0){
+        die("read");
+    }
+    printf("Server says : %s",rbuf);
+
+    close(fd);
+    return  0;
+
+    
+
+
+
+    
+
+    
 }

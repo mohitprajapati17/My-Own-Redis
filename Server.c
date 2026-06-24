3252d85e8c79f8e612fb974792d0f841ae8836e3
mohitprajapati17 <mohitprajapati2444@gmail.com>
2026-06-24 23:28:27 +0530

    server functions

--- Server.c
+++ Server.c
@@ -8,8 +8,39 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 
+
+static void msg(const char *msg){
+    fprintf(stderr,"%s\n",msg);
+}
+
+static void die(const char* msg){
+    int err=errno;
+    fprintf(stderr,"[%d] %s\n",err,msg);
+    abort();
+
+}
+
+static void do_something (int confd){
+     char rbuf[64]={};
+     ssize_t n=read(confd , rbuf , sizeof(rbuf)-1);
+     if(n<0){
+        msg("read() error");
+        return ;
+     }
+     printf(stderr,"Msg from Client: %s\n ", rbuf);
+
+     char ret[]="Hello client";
+     write(confd,ret, strlen(ret));
+}
+
+
 int main(){
 
+    int fd=socket(AF_INET , SOCK_STREAM , 0);
+    if(fd<0){
+        die("socket");
+    }
+    
     
     return 0;
 }

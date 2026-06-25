4332cc2bd83c8156ec13fcb412519a71dfb3b039
mohitprajapati17 <mohitprajapati2444@gmail.com>
2026-06-25 23:58:06 +0530

    client done

--- 02_Client.cpp
+++ 02_Client.cpp
@@ -43,4 +43,89 @@ static int32_t write_all(int fd , const char *buf , size_t n){
     return 0;
 }
 
+
+const size_t k_max_msg=4096;
+
+static  int32_t query(int fd ,const char *text){
+    uint32_t len =(uint32_t)strlen(text);
+
+    if(len>k_max_msg)return -1;
+
+
+    char wbuf[4+len];
+    memcpy(wbuf, &len ,4);
+    memcpy(&wbuf[4] , text , len);
+    if(int32_t err= write_all(fd,wbuf , 4+len)){
+        return err;
+    }
+
+    char rbuf[4+k_max_msg];
+    errno=0;
+    int32_t err= read_full(fd , rbuf, 4);
+    if(err) {
+        msg(errno==0?"EOF":"read() error");
+        return err;
+    }
+
+
+     memcpy(&len, rbuf, 4);  // assume little endian
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+
+
+     // reply body
+    err = read_full(fd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    // do something
+    printf("server says: %.*s\n", len, &rbuf[4]);
+
+
+    return 0;
+
+
+
     
+
+}
+    
+int main(){
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        die("socket()");
+
+    }
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1
+    int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) {
+        die("connect");
+    }
+
+
+     int32_t err = query(fd, "hello1");
+    if (err) {
+        goto L_DONE;
+    }
+    err = query(fd, "hello2");
+    if (err) {
+        goto L_DONE;
+    }
+    err = query(fd, "hello3");
+    if (err) {
+        goto L_DONE;
+    }
+
+    L_DONE:
+    close(fd);
+    return 0;
+}
